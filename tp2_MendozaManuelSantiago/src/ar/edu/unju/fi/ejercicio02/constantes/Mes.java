5d271f5257500e97fe5361b9fb14b4ec10b4caa0
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-27 16:06:33 -0300

    Iniciando Punto Numero 2 del tp2 Mendoza Manuel Santiago

---
+++ tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio02/constantes/Mes.java
@@ -0,0 +1,6 @@
+package ar.edu.unju.fi.ejercicio02.constantes;
+
+public enum Mes {
+	ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+	
+}
---
+++ tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio02/main/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio02.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
