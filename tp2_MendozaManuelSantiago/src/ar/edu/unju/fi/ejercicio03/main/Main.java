26fd64911212124c02f3d526a9c09a30dc7a06d7
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-28 17:36:52 -0300

    se agrego recorrido de la lista de provincias y agregacion de los datos de enum a lista Punto 3 terminado

--- tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -3,15 +3,26 @@ package ar.edu.unju.fi.ejercicio03.main;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//aray para guardar las densidades de las provincias pedidadas  
 		List densidList =new ArrayList();
 		
-     
-        
+		for (Provincia provincia : Provincia.values()) {
+			densidList.add("----------------------------------------------------");
+			densidList.add("Provincia: "+ provincia.name());
+			densidList.add("Poblacion: "+ provincia.getPOBLACION()+ " habitantes");
+			densidList.add("Superficie: "+ provincia.getSUPERFICIE()+" km^2");
+			densidList.add("Densidad: " +provincia.calcularDensidadPoblacional() +" km^2");
+		}
+		
+		System.out.println("************Elementos de la lista:************");
+        for (int i = 0; i < densidList.size(); i++) {
+            System.out.println(densidList.get(i));
+        }
 		
 	}
-
 }
