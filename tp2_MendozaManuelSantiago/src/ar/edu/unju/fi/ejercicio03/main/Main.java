f2637dbd07940ab5b2d7609173d7bca584c2c8a2
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-29 15:06:42 -0300

    punto 3 terminado

--- tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ tp2_MendozaManuelSantiago/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -7,11 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		//aray para guardar las densidades de las provincias pedidadas  
 		Provincia [] procProvincias = Provincia.values();
-		
+		//muestra de datos
 		for (Provincia provincia : procProvincias) {
-			System.out.println("************PROVINCIA:************");
+			System.out.println("************ PROVINCIA: ************");
 			System.out.println("Provincia: "+ provincia.name());
-			System.out.println("Poblacion: "+ provincia.getPOBLACION()+ " habitantes");
+			System.out.println("Poblacion: "+ provincia.getPOBLACION()+ " Habitantes");
 			System.out.println("Superficie: "+ provincia.getSUPERFICIE()+" km^2");
 			System.out.println("Densidad: " +provincia.calcularDensidadPoblacional() +" km^2");
 		}
